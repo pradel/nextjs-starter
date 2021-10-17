55cf2e97534b157eda3c42a6a638b117504d88d7
Pradel Léo <pradel.leo@gmail.com>
2021-10-17 17:57:18 +0200

    feat: setup stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
+import { styled } from '../stitches.config';
+
+const Box = styled('div', { background: '#c7c7c7', color: '#f6f6f6' });
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return <Box>nextjs-starter</Box>;
 };
 
 export default Home;
